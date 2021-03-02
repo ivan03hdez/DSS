fcec682b831731fba32622d56d92836ec4210b8d
ivan <ivan>
2021-03-02 21:18:08 +0100

    promotion borrado

--- DSS/database/migrations/2021_03_02_153020_create_products_table.php
+++ DSS/database/migrations/2021_03_02_153020_create_products_table.php
@@ -23,6 +23,10 @@ class CreateProductsTable extends Migration
             $table->integer('stock');
             $table->string('color');
             $table->string('model');
+            $table->bigInteger('promocion_id')->unsigned()->index();
+            $table->foreign('promocion_id')
+                ->references('id')
+                ->on('promotions');
 
         });
     }
