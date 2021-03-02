e50d951ee30051ff02d9f63d37c7441956d7ae03
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-03-02 19:15:30 +0100

    Añadidas CAj y CP a user y favlist. Ademas de el seeder de users (esta por probar)

---
+++ DSS/database/seeds/UserTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        // Añadimos una entrada a esta tabla
+        
+        for($i = 0; $i < 10; $i++){
+            if ($i == 0)
+            {
+                $role = 'admin';
+            }
+            else
+            {
+                $role = 'user';
+            }
+
+            $phone = 600000000 . $i;
+
+            DB::table('users')->insert([
+                'name' => "Username$i",
+                'email' => "email$i@domain.com",
+                'password' => "pass$i", 
+                'address' => "address$i",
+                'phone' => intval($phone, 10),
+                'role' => $role
+            ]);
+        }
+    }
+}
