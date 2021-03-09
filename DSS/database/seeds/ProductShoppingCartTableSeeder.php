2a4cbc6ff55c66035047595599823e70851ddd45
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-03-09 10:29:25 +0100

    añadidos seeders de las clases M2M, Product_ShoppingCart y Product_FavList

--- DSS/database/seeds/ProductShoppingCartTableSeeder.php
+++ DSS/database/seeds/ProductShoppingCartTableSeeder.php
@@ -9,6 +9,28 @@ class ProductShoppingCartTableSeeder extends Seeder{
      * @return void
      */
     public function run(){
+        
+        DB::table('products_shopping_carts')->delete();
+        $products = DB::table('products')->get();
+        $shopcarts = DB::table('shopping_carts')->get();
+        $pr = array();
+        $sc = array();
+        foreach($products as $product)
+        {
+            array_push($pr, $product);
+        }
 
+        foreach($shopcarts as $shopcart)
+        {
+            array_push($sc, $shopcart);   
+        }
+        $tam = min(count($pr), count($sc));
+        for($i = 0; $i < $tam; $i++)
+        {
+            DB::table('products_shopping_carts')->insert([
+                'product_id' => ($pr[$i]->id),
+                'shopping_cart_id' => ($sc[$i]->id)
+            ]);
+        }
     }
 }
