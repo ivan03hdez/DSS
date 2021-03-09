e1f63be87ea05d2e5a379203cedef14ff2cfe5e0
ivan <ivan>
2021-03-09 10:44:56 +0100

    rehecha pruebas unitarias de producto con model, hay que arreglar error del attach();

--- DSS/tests/Unit/ProductDatabaseTest.php
+++ DSS/tests/Unit/ProductDatabaseTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Product;
+use App\Promotion;
 use DB;
 
 class ProductDatabaseTest extends TestCase{
@@ -14,7 +15,7 @@ class ProductDatabaseTest extends TestCase{
      * @return void
      */
     public function testBasicTest(){
-        $products = DB::table('products')->get();
+        /*$products = DB::table('products')->get();
         $i = 0;
         foreach ($products as $product) {
             echo "$product->promotion_id";
@@ -24,6 +25,34 @@ class ProductDatabaseTest extends TestCase{
             $discounts = DB::table('promotions')->where('id', '=',$product->promotion_id)->get('discount');
             $this->assertEquals(10 +$i,$discounts[0]->discount);
             $i++;
-        }
+        }*/
+        $promotion = new Promotion();
+        $promotion->discount = 25;
+        $promotion->save();
+        $product = new Product();
+        $product->name = 'productoPrueba';
+        $product->price = 35;
+        $product->promotionPrice = 35;
+        $product->color = 'color rojo';
+        $product->description = 'producto de prueba para las pruebas unitarias';
+        $product->model = 'modelo 100';
+        $product->image = '/home/ivan/dss/productImage.png';
+        $product->stock = 100;
+        $product->promotion()->attach($promotion->id);
+        $product->save();
+
+        
+        
+
+
+        echo $product->promotion_id->discount;
+        $this->assertDatabaseHas('products', ['name' => 'productoPrueba', 'image'=> '/home/ivan/dss/productImage.png']);
+        $MaxPrice = Product::max('price');
+        $this->assertEquals(35,$MaxPrice);
+
+
+        $product->delete();
+        $MaxPrice = Product::max('price');
+        $this->assertEquals(24,$MaxPrice);
     }
 }
