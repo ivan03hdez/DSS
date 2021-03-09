be37e31f60f95b65c3fb0fb4f101e9bc6613ad8b
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-03-09 15:18:04 +0100

    Test de FavList hecho

--- DSS/tests/Feature/FavoriteListTest.php
+++ DSS/tests/Feature/FavoriteListTest.php
@@ -5,6 +5,10 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\FavoriteList;
+use App\User;
+use DB;
+
 
 class FavoriteListTest extends TestCase
 {
@@ -25,26 +29,40 @@ class FavoriteListTest extends TestCase
             $User->name = "FavListTest$i";
             $User->email = "FavListTest$i";
             $User->password = "FavListTest$i";
+            $User->phone = "FavListTestPhone$i";
             $User->address = "FavListTest$i";
             $User->role = "user";
             $User->image = "FavListTest$i";
             $User->save();
             
             $FavList->name = "FavListTest$i";
-            $FavList->desciption = "FavListTest$i";
+            $FavList->description = "FavListTest$i";
             $FavList->user_id = $User->id;
+            //$phone->user()->associate($user);
+            $FavList->user()->associate($User);
             $FavList->save();
 
             array_push($uid, $User->id);
             array_push($flid, $FavList->id);
 
+
+
             //Test Create
-            $this->assertDataBaseHas('favorite_lists', []);
+            $this->assertDataBaseHas('favorite_lists', ['id' => $flid[$i-1], 'name' => "FavListTest$i"]);
+            
+            //Test Update
+            $FavList->name = "FavListTestMod$i";
+            $FavList->save();
+            $this->assertDataBaseHas('favorite_lists', ['id' => $flid[$i-1], 'name' => "FavListTestMod$i"]);
+
+            //Probar foreign key
+            $this->assertEquals("FavListTestPhone$i" , $FavList->user->phone);
+            
+            //Test Delete
+            
+            $FavList->delete();
+            $this->assertDatabaseMissing('favorite_lists', ['id' => $flid[$i-1], 'name' => "FavListTestMod$i"]);
         }
-        
-        
-
-
-
     }
 }
+
