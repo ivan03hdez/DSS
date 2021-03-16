656f1663b77ea790112f2e625456558dcb0ed024
ivan <ivan>
2021-03-16 18:01:07 +0100

    arreglados errores de ProductController

--- DSS/app/Http/Controllers/ProductController.php
+++ DSS/app/Http/Controllers/ProductController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\model\User;
+use App\model\Product;
 
 class ProductController extends Controller{
     public function list(){
-        $products = Product::All;
+        $products = Product::all();
         return view('products')->with('products',$products);///si estuviera en una carpeta views/products/list.blade.php seria view('product.list')
     }
     public function get($id){
-        $productDetails = Product::Where('id',$id);
+        $productDetails = Product::where('id',$id);
         return view('product')->with('product',$productDetails);
     }
     public function create(){
