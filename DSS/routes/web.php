d429ccc8bfe24e15ee5f723baa1b3dc9e8d87110
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-03-16 19:29:41 +0100

    Cosas random, pruebas sin resultados

--- DSS/routes/web.php
+++ DSS/routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 Route::get('/products', 'ProductController@list');
 Route::get('/products/{id}', 'ProductController@get');
 Route::get('/users', 'UserControllr@list');
+
+
+
+Route::get('/', function(){return view('exampleView');});
