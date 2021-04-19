09c08a830db1212ae8b2e566313b861ce12ec266
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-04-19 14:58:40 +0200

    Cambios en el footer para hacerlo sticky en el bottom

--- DSS/resources/views/layouts/admin.blade.php
+++ DSS/resources/views/layouts/admin.blade.php
@@ -91,7 +91,7 @@
             </div>
         </footer>
 -->
-    <footer class="bg-light text-center text-white">
+    <footer class="bg-light text-center text-white fixed-bottom ">
       <!-- Grid container -->
       <div class="container p-4 pb-0">
       Redes sociales:
@@ -106,8 +106,8 @@
 
       <!-- Copyright -->
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        © 2021 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/">DEAF SL</a>
       </div>
       
       
