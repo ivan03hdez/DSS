a34460e13d257d5310a8d1aee44fd289e176053c
Diego Barreno Intriago <dbi4@alu.ua.es>
2021-05-03 14:38:49 +0200

    no se

--- DSS/resources/views/login.blade.php
+++ DSS/resources/views/login.blade.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
@@ -13,9 +14,12 @@
 
   </head>
 
-  <body class="text-center body-log">
-    <form class="form-signin" method="POST" action="{{ route('login') }}">
-      <img class="mb-4 img-fluid" src="{{ URL::asset('images/deaf.jpeg') }}" alt="" width="300" height="300">    
+  <!--<body class="text-center body-log">-->
+  <body class="text-center">
+  <div class="container-fluid">
+  <div class="row">
+    <form class="form-signin form-inline my-2 my-lg-0 ml-auto" method="POST" action="{{ route('login') }}">
+                <img  src="{{ URL::asset('images/deaf.jpeg') }}"  alt="" title="">
       <h1 class="h3 mb-3 font-weight-normal">Welcome</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Email address" required autofocus>
@@ -45,6 +49,8 @@
       @endif
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
+    </div>
+    </div>
     <a class="nav-link" href="{{ route('login') }}">
       {{ __('Login') }}
     </a>
@@ -52,8 +58,10 @@
     <a class="nav-link" href="{{ route('register') }}">
       {{ __('Register') }}
     </a>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <script type="text/javascript" src="{{ URL::asset('JS/script.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   </body>
+  
 </html>
