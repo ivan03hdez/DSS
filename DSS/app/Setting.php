4d7ea54a5fc56420b3c70c933fe57dbb74bb8902
jesus <jaf28@gcloud.ua.es>
2021-05-11 18:42:02 +0200

    Actualizado modelo settings

--- DSS/app/Setting.php
+++ DSS/app/Setting.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'settings';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['key', 'value'];
 }
