1af01f659c1e3c3c3fe40fc2e2069f17aef860d4
jesus <jaf28@gcloud.ua.es>
2021-05-11 18:41:06 +0200

    Actualizada migracion settings

--- DSS/database/migrations/2021_05_11_163822_create_settings_table.php
+++ DSS/database/migrations/2021_05_11_163822_create_settings_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateSettingsTable extends Migration
 {
@@ -15,6 +15,8 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('key')->unique();
+            $table->text('value')->nullable();
             $table->timestamps();
         });
     }
