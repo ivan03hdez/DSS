cbc82ba80df770f195f42ac9854518be493e07eb
jesus <jaf28@gcloud.ua.es>
2021-05-11 17:06:10 +0200

    Actualizadas rutas para admin en admin.php

--- DSS/routes/admin.php
+++ DSS/routes/admin.php
@@ -1,4 +1,13 @@
 <?php
 
-Route::view('/admin', 'admin.dashboard.index');
-Route::view('/admin/login', 'admin.auth.login');
+Route::group(['prefix'  =>  'admin'], function () {
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
+    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+    Route::get('/', function () {
+        return view('admin.dashboard.index');
+    });
+
+});
