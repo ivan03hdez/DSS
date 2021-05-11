d4d8fbbf6a59de92b39dfd34287de56458ed4224
jesus <jaf28@gcloud.ua.es>
2021-05-11 16:49:15 +0200

    Añadidos guards para Admin en auth.php

--- DSS/config/auth.php
+++ DSS/config/auth.php
@@ -40,10 +40,21 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
+    
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
+            'hash' => false,
+        ],
+    
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+    
+        'admin-api' => [
+            'driver' => 'token',
+            'provider' => 'admins',
             'hash' => false,
         ],
     ],
