4d9f1601dc1c1976ca5c1841b107681c529638db
jesus <jaf28@gcloud.ua.es>
2021-05-12 12:58:30 +0200

    Actualizado seeder Categoría

--- DSS/database/factories/CategoryFactory.php
+++ DSS/database/factories/CategoryFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\AppCategory;
+use App\Category;
 use Faker\Generator as Faker;
 
-$factory->define(AppCategory::class, function (Faker $faker) {
+$factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'name'          =>  $faker->name,
+        'description'   =>  $faker->realText(100),
+        'parent_id'     =>  1,
+        'menu'          =>  1,
     ];
 });
