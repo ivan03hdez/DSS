89fff619708d95e3671faf52ffa8faf171b8f84d
jesus <jaf28@gcloud.ua.es>
2021-05-12 16:24:16 +0200

    creado seeder para Attribute

--- DSS/app/Attribute.php
+++ DSS/app/Attribute.php
@@ -4,7 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Attribute
+ * @package App\Models
+ */
 class Attribute extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
 }
