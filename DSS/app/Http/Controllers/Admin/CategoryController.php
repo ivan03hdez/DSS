ac124a5b3ed37c293e6179e01977e7c6dfe76a3c
jesus <jaf28@gcloud.ua.es>
2021-05-12 13:10:33 +0200

    Creado controlador para categoría

---
+++ DSS/app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+
+/**
+ * Class CategoryController
+ * @package App\Http\Controllers\Admin
+ */
+class CategoryController extends BaseController
+{
+    //
+}
