c6b6692ea225afc65fb3eb7b6bc91d7abe54f854
jesus <jaf28@gcloud.ua.es>
2021-05-12 13:27:47 +0200

    Actualizado controlador Categoría

--- DSS/app/Http/Controllers/Admin/CategoryController.php
+++ DSS/app/Http/Controllers/Admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
+use App\Contracts\CategoryContract;
 
 /**
  * Class CategoryController
@@ -11,5 +12,28 @@ use App\Http\Controllers\BaseController;
  */
 class CategoryController extends BaseController
 {
-    //
+    /**
+     * @var CategoryContract
+     */
+    protected $categoryRepository;
+
+    /**
+     * CategoryController constructor.
+     * @param CategoryContract $categoryRepository
+     */
+    public function __construct(CategoryContract $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $categories = $this->categoryRepository->listCategories();
+
+        $this->setPageTitle('Categories', 'List of all categories');
+        return view('admin.categories.index', compact('categories'));
+    }
 }
