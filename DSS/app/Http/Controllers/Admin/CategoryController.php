1a61b2d84ee39429441f735b3d54c5373a4011c2
jesus <jaf28@gcloud.ua.es>
2021-05-12 16:10:26 +0200

    añadida función para editar categoría

--- DSS/app/Http/Controllers/Admin/CategoryController.php
+++ DSS/app/Http/Controllers/Admin/CategoryController.php
@@ -47,4 +47,40 @@ class CategoryController extends BaseController
         $this->setPageTitle('Categories', 'Create Category');
         return view('admin.categories.create', compact('categories'));
     }
+
+    /**
+    * @param Request $request
+    * @return \Illuminate\Http\RedirectResponse
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->createCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while creating category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category added successfully' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+    */
+    public function edit($id)
+    {
+        $targetCategory = $this->categoryRepository->findCategoryById($id);
+        $categories = $this->categoryRepository->listCategories();
+
+        $this->setPageTitle('Categories', 'Edit Category : '.$targetCategory->name);
+        return view('admin.categories.edit', compact('categories', 'targetCategory'));
+    }
 }
